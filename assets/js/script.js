808d6f415906e42e29182f292a7615b15913ced3
MasakiDesktop <mandij50@gmail.com>
2023-01-23 21:13:49 -0500

    searching shows forecast too

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,30 +99,69 @@ function cityForecast(city) {
         console.log(data)
         makeCards();
 
-        // var forecastDateEl = document.getElementById(`forecastDate${0}`);
-        // var forecastDateEl1 = document.getElementById(`forecastDate${1}`);
-        // var forecastDateEl2 = document.getElementById(`forecastDate${2}`);
-        // var forecastDateEl3 = document.getElementById(`forecastDate${3}`);
-        // var forecastDateEl4 = document.getElementById(`forecastDate${4}`);
+        // filling in the date for each card in the forecast
+        var forecastDateEl = document.getElementById(`forecastDate${0}`);
+        var forecastDateEl1 = document.getElementById(`forecastDate${1}`);
+        var forecastDateEl2 = document.getElementById(`forecastDate${2}`);
+        var forecastDateEl3 = document.getElementById(`forecastDate${3}`);
+        var forecastDateEl4 = document.getElementById(`forecastDate${4}`);
 
-        // var forecastDate = todayDate.add(1, 'day').format('MM~D~YYYY');
-        // var forecastDate1 = todayDate.add(2, 'day').format('MM~D~YYYY');
-        // var forecastDate2 = todayDate.add(3, 'day').format('MM~D~YYYY');
-        // var forecastDate3 = todayDate.add(4, 'day').format('MM~D~YYYY');
-        // var forecastDate4 = todayDate.add(5, 'day').format('MM~D~YYYY');
+        var forecastDate = todayDate.add(1, 'day').format('MM~D~YYYY');
+        var forecastDate1 = todayDate.add(2, 'day').format('MM~D~YYYY');
+        var forecastDate2 = todayDate.add(3, 'day').format('MM~D~YYYY');
+        var forecastDate3 = todayDate.add(4, 'day').format('MM~D~YYYY');
+        var forecastDate4 = todayDate.add(5, 'day').format('MM~D~YYYY');
 
-        // forecastDateEl.textContent = forecastDate;
-        // forecastDateEl1.textContent = forecastDate1;
-        // forecastDateEl2.textContent = forecastDate2;
-        // forecastDateEl3.textContent = forecastDate3;
-        // forecastDateEl4.textContent = forecastDate4;
+        forecastDateEl.textContent = forecastDate;
+        forecastDateEl1.textContent = forecastDate1;
+        forecastDateEl2.textContent = forecastDate2;
+        forecastDateEl3.textContent = forecastDate3;
+        forecastDateEl4.textContent = forecastDate4;
 
-        // // console.log(data.list[6].weather[0].icon);
-        // displayWeatherIcon(forecastIcon0, data.list[6].weather[0].icon);
-        // displayWeatherIcon(forecastIcon1, data.list[14].weather[0].icon);
-        // displayWeatherIcon(forecastIcon2, data.list[22].weather[0].icon);
-        // displayWeatherIcon(forecastIcon3, data.list[30].weather[0].icon);
-        // displayWeatherIcon(forecastIcon4, data.list[38].weather[0].icon);
+        // filling in the icon for the forecast
+        displayWeatherIcon(forecastIcon0, data.list[6].weather[0].icon);
+        displayWeatherIcon(forecastIcon1, data.list[14].weather[0].icon);
+        displayWeatherIcon(forecastIcon2, data.list[22].weather[0].icon);
+        displayWeatherIcon(forecastIcon3, data.list[30].weather[0].icon);
+        displayWeatherIcon(forecastIcon4, data.list[38].weather[0].icon);
+
+        // filling in the forecast weather type
+        var weatherType = document.getElementById(`weatherType${0}`);
+        var weatherType1 = document.getElementById(`weatherType${1}`);
+        var weatherType2 = document.getElementById(`weatherType${2}`);
+        var weatherType3 = document.getElementById(`weatherType${3}`);
+        var weatherType4 = document.getElementById(`weatherType${4}`);
+
+        weatherType.textContent = data.list[6].weather[0].description;
+        weatherType1.textContent = data.list[14].weather[0].description;
+        weatherType2.textContent = data.list[22].weather[0].description;
+        weatherType3.textContent = data.list[30].weather[0].description;
+        weatherType4.textContent = data.list[38].weather[0].description;
+    
+        // filling in the temp in the forecast cards
+        var forecastTemp = document.getElementById(`forecastTemp${0}`)
+        var forecastTemp1 = document.getElementById(`forecastTemp${1}`)
+        var forecastTemp2 = document.getElementById(`forecastTemp${2}`)
+        var forecastTemp3 = document.getElementById(`forecastTemp${3}`)
+        var forecastTemp4 = document.getElementById(`forecastTemp${4}`)
+
+        forecastTemp.textContent = Math.round(data.list[6].main.temp) + '°F';
+        forecastTemp1.textContent = Math.round(data.list[14].main.temp) + '°F';
+        forecastTemp2.textContent = Math.round(data.list[22].main.temp) + '°F';
+        forecastTemp3.textContent = Math.round(data.list[30].main.temp) + '°F';
+        forecastTemp4.textContent = Math.round(data.list[38].main.temp) + '°F';
+
+        // Humidity for the forecast, why? I dunno, who uses this?
+        var forecastHumi = document.getElementById(`forecastHumi${0}`)
+        var forecastHumi1 = document.getElementById(`forecastHumi${1}`)
+        var forecastHumi2 = document.getElementById(`forecastHumi${2}`)
+        var forecastHumi3 = document.getElementById(`forecastHumi${3}`)
+        var forecastHumi4 = document.getElementById(`forecastHumi${4}`)
+        forecastHumi.textContent = data.list[6].main.humidity + '%';
+        forecastHumi1.textContent = data.list[14].main.humidity + '%';
+        forecastHumi2.textContent = data.list[22].main.humidity + '%';
+        forecastHumi3.textContent = data.list[30].main.humidity + '%';
+        forecastHumi4.textContent = data.list[38].main.humidity + '%';
     })
 }
 
